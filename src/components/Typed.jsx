b98e3936a209379aa519e239689dcedc780cc76b
DemuraAIdev <cyberaioff@gmail.com>
2023-01-25 12:10:28 +0700

    Lol

--- src/components/Typed.jsx
+++ src/components/Typed.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Typed from "typed.js";
-import useTranslation from "next-translate/useTranslation";
 export default function TypedBios() {
     let el = React.useRef(null);
     let typed = React.useRef(null);
@@ -21,13 +20,10 @@ export default function TypedBios() {
         <>
             <ul id="bios" className="hidden">
                 <li>
-                    {t("Live")} <b className="font-medium">Indonesia</b>.
-                </li>
-                <li>
-                    {t("myfirstprogram")} <b className="font-medium">HTML</b>.
+                    I live in indonesia
                 </li>
                 <li>I love NODEJS.</li>
-                <li>{t("workmost")}</li>
+                <li>This webiste under construction 🚧</li>
                 <li>I love EDM music.</li>
             </ul>
             <span
