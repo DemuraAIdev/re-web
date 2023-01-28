a1d23a182cb692ee7e8104988e2bb21ddcb61161
DemuraAIdev <cyberaioff@gmail.com>
2023-01-28 19:32:18 +0700

    Update Types For MDXCompo

--- src/components/MDXCompo.tsx
+++ src/components/MDXCompo.tsx
@@ -1,5 +1,29 @@
 import Image from "next/image";
-import { getMDXComponent } from "mdx-bundler/client";
+import { getMDXComponent, ComponentMap } from "mdx-bundler/client";
 import Link from "./CustomLink";
 import TOCInline from "./TOCInline";
 import Pre from "./Pre";
+import PostLayout from "@/layouts/PostLayout";
+import { useMemo } from "react";
+
+export const MDXComponents: ComponentMap = {
+    TOCInline,
+    Image,
+    a: Link,
+    pre: Pre,
+    wrapper: PostLayout,
+};
+
+export const MDXLayoutRenderer = ({
+    layout,
+    mdxSource,
+    ...rest
+}: {
+    layout: any;
+    mdxSource: string;
+    [key: string]: any;
+}) => {
+    const MDXLayout = useMemo(() => getMDXComponent(mdxSource), [mdxSource]);
+
+    return <MDXLayout layout={layout} components={MDXComponents} {...rest} />;
+};
--- src/pages/blog/[...slug].tsx
+++ src/pages/blog/[...slug].tsx
@@ -1,6 +1,9 @@
 import PageTitle from "@/components/PageTitle";
 import generateRss from "@/lib/generate-rss";
+import fs from "fs";
 import { formatSlug, getAllFilesFrontMatter, getFileBySlug, getFiles } from "@/lib/mdx";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { MDXLayoutRenderer } from "@/components/MDXCompo";
 
 export async function getStaticPaths() {
     const posts = getFiles("blog");
@@ -12,4 +15,40 @@ export async function getStaticPaths() {
         })),
         fallback: false,
     };
+}
+
+export async function getStaticProps({ params }: GetStaticPropsContext) {
+    const allPosts = await getAllFilesFrontMatter('blog')
+    const post = await getFileBySlug("blog", params?.slug as string);
+    const rss = generateRss(allPosts)
+    fs.writeFileSync(`./public/feed.xml`, rss)
+
+    return { props: { post } };
+
+}
+const DEFAULT_LAYOUT = "PostLayout";
+
+export default function Blog({ post }: InferGetStaticPropsType<typeof getStaticProps>) {
+    const { mdxSource, toc, frontMatter } = post;
+    return (
+        <>
+            {!frontMatter.draft ? (
+                <MDXLayoutRenderer
+                    layout={frontMatter.layout || DEFAULT_LAYOUT}
+                    toc={toc}
+                    mdxSource={mdxSource}
+                    frontMatter={frontMatter}
+                />
+            ) : (
+                <div className="mt-24 text-center">
+                    <PageTitle>
+                        Under Construction{" "}
+                        <span role="img" aria-label="roadwork sign">
+                            🚧
+                        </span>
+                    </PageTitle>
+                </div>
+            )}
+        </>
+    );
 }
