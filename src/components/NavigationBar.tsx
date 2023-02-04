91ce38dba94a870ee874046dca58a28c98d8f93c
DemuraAIdev <cyberaioff@gmail.com>
2023-02-04 20:16:29 +0700

    backnormal

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -45,7 +45,7 @@ function NavItem({ href, text }: { href: string, text: string }) {
             href={href}
             className={
                 isActive
-                    ? "rounded-lg p-1 font-semibold text-black transition-all hover:scale-125 hover:bg-gray-200 hover:text-black dark:text-white  dark:hover:bg-gray-800 dark:hover:text-white sm:px-3 sm:py-2 md:inline-block border-dashed  border border-gray-500"
+                    ? "rounded-lg p-1 font-semibold text-black transition-all hover:scale-125 hover:bg-gray-200 hover:text-black dark:text-white  dark:hover:bg-gray-800 dark:hover:text-white sm:px-3 sm:py-2 md:inline-block "
                     : "rounded-lg p-1 font-medium text-gray-500 transition-all hover:scale-125 hover:bg-gray-200 hover:text-black dark:text-gray-400  dark:hover:bg-gray-800 dark:hover:text-white sm:px-3 sm:py-2 md:inline-block"
             }
         >
