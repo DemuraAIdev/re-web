f90aa5e894a0381e4ef833fa11f8f40bd3b7ef21
DemuraAIdev <cyberaioff@gmail.com>
2023-02-04 13:07:33 +0700

    header update

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,10 +10,20 @@ export function middleware(request: NextRequest) {
             headers: requestHeaders,
         },
     })
+    const ContentSecurityPolicy = `
+        default-src 'self';
+        script-src 'self' 'unsafe-eval' 'unsafe-inline' *.vahryiskandar.my.id;
+        child-src  *.youtube.com *.google.com *.twitter.com giscus.app;
+        style-src 'self';
+        font-src 'self';  
+    `
     response.headers.set('X-Next-Server', 'Next.js')
-    
+    response.headers.set('X-Frame-Options', 'DENY')
+    response.headers.set('Cache-Control', 'must-revalidate, max-age=3600')
+    response.headers.set('Content-Security-Policy', ContentSecurityPolicy.replace(/\n/g, ""))
+
 
     return response
 
-    
+
 }
